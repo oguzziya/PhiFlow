42ae21a90ad5b1dcbe2ec99fd040ab3da1065c72
Philipp <Philipp@MHoll.de>
2020-07-08 13:32:10 +0200

    Explicit simpleplume.py concept

--- demos/simpleplume.py
+++ demos/simpleplume.py
@@ -1,6 +1,16 @@
 from phi.flow import *
+# physics_config.x_first()
 
-world.add(Fluid(Domain([80, 64], boundaries=CLOSED), buoyancy_factor=0.1), physics=IncompressibleFlow())
-world.add(Inflow(Sphere(center=(10, 32), radius=5), rate=0.2))
+domain = Domain([80, 64], boundaries=CLOSED)
+dt = 1.0
+buoyancy_factor = 0.1
+inflow = mask(Sphere(center=(10, 32), radius=5)) * 0.2
+velocity = StaggeredGrid.sample(0, domain)
+density = CenteredGrid.sample(0, domain)
 
-show(App('Simple Plume', framerate=10))
+app = show(App('Simple Plume', framerate=10, fields={'velocity': velocity, 'density': density}))
+while True:
+    density = advect.semi_lagrangian(density, velocity, dt) + inflow
+    velocity = advect.semi_lagrangian(velocity, velocity, dt) + density * (buoyancy_factor, 0)
+    velocity = divergence_free(velocity, domain)
+    app.update({'velocity': velocity, 'density': density})
